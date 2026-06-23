a48c3422126044787de600f4da3b06907ec38453
Hirintsoa <hirintsoa@average-engineer.com>
2026-06-23 20:51:23 +0300

    refract 0.1.0-rc1

--- Formula/refract.rb
+++ Formula/refract.rb
@@ -1,26 +1,26 @@
 class Refract < Formula
   desc "Fast Ruby language server backed by SQLite"
   homepage "https://github.com/hrtsx/refract"
-  version "0.1.0-rc.1"
+  version "0.1.0-rc1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/hrtsx/refract/releases/download/v#{version}/refract-aarch64-macos"
-      sha256 "664108f24a6f83dab4402d4505ad060e1a7b4fda5c9bebebd5fa2a188d82c7dc"
+      sha256 "59fb86c654f66cf0602b0e8901277599749622220e53ac44029e1663796c18a3"
     else
       url "https://github.com/hrtsx/refract/releases/download/v#{version}/refract-x86_64-macos"
-      sha256 "357e34675e417975c7a17eaa96c1b4e3b55c1f0d8f6f58a534b3fe618fbad2c4"
+      sha256 "195a2cecd89a5af4a9e5d3ed819d799b7a3a0559724e61405bdcc431bde2bb78"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/hrtsx/refract/releases/download/v#{version}/refract-aarch64-linux"
-      sha256 "12851257c9e961aae8c7c100f12b6cd9df133d6b1ce7c8e90d863a67829d0fc0"
+      sha256 "b6132fa3fb760a2e91f5bacf72e9b08acd3eeb0e6459097ff3d171c38bc929d4"
     else
       url "https://github.com/hrtsx/refract/releases/download/v#{version}/refract-x86_64-linux"
-      sha256 "206790be2b89b68ee73d2143f98d9bf39b2821f133971c102df993e712fdb849"
+      sha256 "2b61f4312b6dfc3b6e4e2f52bf47f11ee516136d6e1a3a9bf1f2f2ad32e63d97"
     end
   end
 
